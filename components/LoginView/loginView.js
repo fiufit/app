f5449e1455a93efb534fd0b0a5179b09e565d7de
fpenic <fpenic@quadiontech.com>
2023-03-28 21:37:16 -0300

    Style loginView component.

--- components/LoginView/loginView.js
+++ components/LoginView/loginView.js
@@ -1,8 +1,8 @@
 import { Button, TextInput } from "react-native-paper";
+import { Image, Text, View } from "react-native";
 
 import Background from "../Background/background";
 import React from "react";
-import { Text } from "react-native";
 import { styles } from "./styles.loginView";
 
 const LoginView = () => {
@@ -13,21 +13,39 @@ const LoginView = () => {
       <TextInput
         style={styles.textInput}
         placeholder="Email"
+        activeUnderlineColor="#202020"
         left={<TextInput.Icon icon="email" />}
       ></TextInput>
       <TextInput
         style={styles.textInput}
+        activeUnderlineColor="#202020"
         placeholder="Password"
         right={<TextInput.Icon icon="eye" />}
+        left={<TextInput.Icon icon="lock" />}
       ></TextInput>
+      <View style={styles.forgotPasswordContainer}>
+        <Text style={styles.forgotPasswordText}>Forgot your password?</Text>
+      </View>
       <Button
         icon="login"
         mode="contained"
+        buttonColor="#202020"
         style={styles.loginButton}
         onPress={() => console.log("Pressed")}
       >
         Log In
       </Button>
+      <Text style={styles.orText}>Or</Text>
+      <Image
+        style={styles.googleImage}
+        source={require("../../assets/googleIcon.png")}
+      />
+      <View style={styles.doNotHaveAccountContainer}>
+        <Text style={styles.doNotHaveAccountText}>
+          Don't have an account yet?{" "}
+          <Text style={styles.register}>Register</Text>{" "}
+        </Text>
+      </View>
     </Background>
   );
 };
--- components/LoginView/styles.loginView.js
+++ components/LoginView/styles.loginView.js
@@ -8,14 +8,41 @@ export const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 40,
   },
   textInput: {
-    width: 200,
+    width: "80%",
     height: 30,
     marginTop: 20,
   },
   loginButton: {
-    marginTop: 20,
-    width: 200,
+    marginTop: 50,
+    width: "80%",
+  },
+  forgotPasswordContainer: {
+    width: "80%",
+    marginTop: 7,
+  },
+  forgotPasswordText: {
+    marginLeft: "auto",
+    color: "#ADA4A5",
+    textDecorationLine: "underline",
+  },
+  orText: {
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  googleImage: {
+    width: 40,
+    height: 40,
+  },
+  doNotHaveAccountContainer: {
+    marginTop: 50,
+  },
+  doNotHaveAccountText: {
+    fontSize: 15,
+  },
+  register: {
+    color: "#9CB351",
   },
 });
