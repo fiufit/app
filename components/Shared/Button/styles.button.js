f5a51e6305bef6827a60512e026daca7b08bf49b
Simon Stein <sstein@fi.uba.ar>
2023-04-04 19:35:17 -0300

    Create Button component

---
+++ components/Shared/Button/styles.button.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    buttonContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        textAlign: "center",
+        gap: 10,
+        backgroundColor: "#202020"
+    },
+    textContainer: {
+    },
+    input: {
+        textAlignVertical: "center",
+        width: "70%",
+        paddingBottom: 0,
+    }
+});
