1ebb15cd57a29df4708519f766717db814281113
Simon Stein <sstein@fi.uba.ar>
2023-04-09 21:31:31 -0300

    Create signOut and signIn functions

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 
-import {getAuth, signInWithEmailAndPassword, initializeAuth} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword, initializeAuth, signOut} from "firebase/auth";
 import { getReactNativePersistence } from "firebase/auth/react-native"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 const firebaseConfig = {
@@ -19,4 +19,19 @@ initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage)
 });
 export const auth = getAuth();
-export const singIn = async (email, password) => await signInWithEmailAndPassword(auth, email, password);
+export const singIn = async (email, password) => {
+    try{
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (e) {
+        // TODO: change alert for an error modal with error interpretation
+        alert(e)
+    }
+};
+export const signOutFromApp = async (onSingOut) => {
+    await signOut(auth);
+    onSingOut();
+};
+
+export const signInWithGoogle = async () => {
+    //TODO
+}
