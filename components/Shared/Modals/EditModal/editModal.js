67bce67c19ff39302e76d6028eb00ec61b60edbd
Simon Stein <sstein@fi.uba.ar>
2023-04-12 12:41:27 -0300

    Add inputMode prop to EditModal

--- components/Shared/Modals/EditModal/editModal.js
+++ components/Shared/Modals/EditModal/editModal.js
@@ -6,13 +6,13 @@ import Button from "../../Button/button";
 import {WHITE} from "../../../../utils/colors";
 import {useState} from "react";
 
-const EditModal = ({title, editValue, editPlaceHolder, editIcon, buttonText, onButtonPress, setEditValue}) => {
+const EditModal = ({title, editValue, editPlaceHolder, editIcon, buttonText, onButtonPress, setEditValue, inputMode}) => {
     const [text, setText] = useState(editValue)
     return(
         <View style={styles.modalContainer}>
             <View style={styles.modal}>
                 <Text style={styles.title}>{title}</Text>
-                <Input placeholder={editPlaceHolder} value={text} left={editIcon} width={"100%"} height={40} marginTop={0} onChangeText={(text) => setText(text)}/>
+                <Input placeholder={editPlaceHolder} value={text} left={editIcon} width={"100%"} height={40} marginTop={0} onChangeText={(text) => setText(text)} inputMode={inputMode}/>
                 <Button textColor={WHITE} style={styles.button} onPress={() => {
                     if(text.length) {
                         setEditValue(text);
