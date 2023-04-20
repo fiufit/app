007febaf8b498959eb00b5180b0d1254b43c8039
Simon Stein <sstein@fi.uba.ar>
2023-04-20 19:00:23 -0300

    Create FavouriteTrainings component

---
+++ components/Trainings/FavouriteTrainings/favouriteTrainings.js
@@ -0,0 +1,14 @@
+import {View, Text} from "react-native";
+import {styles} from "./style.favourite-trainings";
+
+const FavouriteTrainings = () => {
+
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Your favourites</Text>
+        </View>
+    )
+}
+
+export default FavouriteTrainings;
---
+++ components/Trainings/FavouriteTrainings/style.favourite-trainings.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "90%",
+        height: 220
+    },
+    title: {
+        fontFamily: "Lato_700Bold",
+        fontSize: 18
+    }
+});
