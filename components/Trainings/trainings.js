fdf9be72241065dc89e26faefe12a6d97cc8bd5f
Simon Stein <sstein@fi.uba.ar>
2023-04-20 19:00:48 -0300

    Add new components to trainings view

--- components/Trainings/trainings.js
+++ components/Trainings/trainings.js
@@ -1,23 +1,29 @@
-import {View} from "react-native";
+import {ScrollView, View, Text} from "react-native";
 import Input from "../Shared/Input/input";
 import {styles} from "./style.trainings";
 import Button from "../Shared/Button/button";
 import {GREEN, WHITE} from "../../utils/colors";
 import SearchIcon from '../../assets/images/general/searchIcon.svg'
 import FilterIcon from '../../assets/images/general/filterIcon.svg'
+import RecommendedTraining from "./RecommendedTraining/recommendedTraining";
+import FavouriteTrainings from "./FavouriteTrainings/favouriteTrainings";
 const Trainings = () => {
 
     return (
         <View style={styles.container}>
             <View style={styles.inputAndButtonContainer}>
                 <Input placeholder={"Search"} height={48} width={"82%"} marginTop={0} backgroundColor={WHITE}
-                       left={<SearchIcon/> }
+                       left={<SearchIcon/>}
                        right={<FilterIcon/>}
                         fontFamily={"Lato_400Regular"} fontSize={14}
                 />
                 <Button style={styles.addButton} buttonColor={GREEN} fontSize={25}
                         fontFamily={"Lato_400Regular"}>+</Button>
             </View>
+            <ScrollView style={{width: "100%", display: "flex", flexDirection: "column"}} contentContainerStyle={{justifyContent: "center", alignItems: "center"}}>
+                <RecommendedTraining/>
+                <FavouriteTrainings/>
+            </ScrollView>
 
         </View>
     )
