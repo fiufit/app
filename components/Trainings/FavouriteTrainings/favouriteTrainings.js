95eff6330a4463f0521a70cdd1bb7c9f69ee0a70
Simon Stein <sstein@fi.uba.ar>
2023-04-24 23:26:50 -0300

    Add favourite training cards

--- components/Trainings/FavouriteTrainings/favouriteTrainings.js
+++ components/Trainings/FavouriteTrainings/favouriteTrainings.js
@@ -1,12 +1,33 @@
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity, ScrollView} from "react-native";
 import {styles} from "./style.favourite-trainings";
+
 
 const FavouriteTrainings = () => {
 
 
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Your favourites</Text>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>Favourite Trainings</Text>
+                <TouchableOpacity>
+                    <Text style={styles.seeAll}>See all</Text>
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView horizontal style={styles.trainingsContainer} showsHorizontalScrollIndicator={false}>
+                <View style={styles.cardColumn}>
+                    <View style={styles.trainingCard}>
+                    </View>
+                    <View style={styles.trainingCard}>
+                    </View>
+                </View>
+                <View style={styles.cardColumn}>
+                    <View style={styles.trainingCard}>
+                    </View>
+                    <View style={styles.trainingCard}>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
--- components/Trainings/FavouriteTrainings/style.favourite-trainings.js
+++ components/Trainings/FavouriteTrainings/style.favourite-trainings.js
@@ -1,12 +1,42 @@
 import { StyleSheet } from "react-native";
+import {MEDIUM_GREY, WHITE} from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
         width: "90%",
-        height: 220
+        height: 220,
+        display: "flex",
+        gap: 10
     },
     title: {
         fontFamily: "Lato_700Bold",
-        fontSize: 18
+        fontSize: 18,
+    },
+    textContainer: {
+        display: "flex",
+        flexDirection: "row",
+        width: "96%",
+        alignItems: 'flex-end',
+        justifyContent: "space-between",
+    },
+    seeAll: {
+        fontSize: 13.5,
+        fontFamily: "Lato_700Bold",
+        color: MEDIUM_GREY
+    },
+    trainingsContainer: {
+        display: "flex",
+        flexDirection: "row",
+    },
+    trainingCard: {
+        width: 200,
+        height: "45%",
+        borderRadius: 9,
+        backgroundColor: WHITE
+    },
+    cardColumn: {
+        display: "flex",
+        gap: 10,
+        marginRight: 15,
     }
 });
