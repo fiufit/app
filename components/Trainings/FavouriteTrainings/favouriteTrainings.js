be070d8b3f6316db496b6838a4b2e5e2121e03ed
Simon Stein <sstein@fi.uba.ar>
2023-04-25 23:22:42 -0300

    Add favourite trainings cards

--- components/Trainings/FavouriteTrainings/favouriteTrainings.js
+++ components/Trainings/FavouriteTrainings/favouriteTrainings.js
@@ -1,32 +1,93 @@
-import {View, Text, TouchableOpacity, ScrollView} from "react-native";
+import {View, Text, TouchableOpacity, ScrollView, Image} from "react-native";
 import {styles} from "./style.favourite-trainings";
+import trainingImage from "../../../assets/images/examples/woman.png";
+import {useEffect, useState} from "react";
 
 
 const FavouriteTrainings = () => {
+    const [trainingsToShow, setTrainingsToShow] = useState([]);
+    const favouriteTrainings = [
+        {
+            title: 'Belly fat burner',
+            duration: '10 min',
+            difficulty: 'Begineer',
+            id: 1
+        },
+        {
+            title: 'Belly fat burner',
+            duration: '10 min',
+            difficulty: 'Begineer',
+            id: 2
+        },
+        {
+            title: 'Belly fat burner',
+            duration: '10 min',
+            difficulty: 'Begineer',
+            id: 3
+        },
+        {
+            title: 'Belly fat burner',
+            duration: '10 min',
+            difficulty: 'Begineer',
+            id: 4
+        },
+        {
+            title: 'Belly fat burner',
+            duration: '10 min',
+            difficulty: 'Begineer',
+            id: 5
+        }
+    ];
+    useEffect(() => {
+        //TODO fetch trainings from db and if there are not favourites show recommended
+        setTrainingsToShow(favouriteTrainings)
+    }, [])
 
 
-    return(
+    const transformTrainings = (trainings) => {
+        const transformedTrainings = [];
+        if (trainings.length <= 2) {
+            return trainings.map(training => [training])
+        }
+        for (let i = 0; i < trainings.length; i += 2) {
+            if (i + 1 < trainings.length) {
+                transformedTrainings.push([trainings[i], trainings[i + 1]]);
+            } else {
+                transformedTrainings.push([trainings[i]]);
+            }
+        }
+        return transformedTrainings;
+    }
+
+
+    return (
         <View style={styles.container}>
             <View style={styles.textContainer}>
-                <Text style={styles.title}>Favourite Trainings</Text>
+                <Text style={styles.title}>
+                    {favouriteTrainings.length ? 'Favourite Trainings' : 'Add these to favourites'}
+                </Text>
                 <TouchableOpacity>
-                    <Text style={styles.seeAll}>See all</Text>
+                    <Text style={styles.seeAll}>{favouriteTrainings.length ? 'See all' : ''}</Text>
                 </TouchableOpacity>
             </View>
 
             <ScrollView horizontal style={styles.trainingsContainer} showsHorizontalScrollIndicator={false}>
-                <View style={styles.cardColumn}>
-                    <View style={styles.trainingCard}>
+                {transformTrainings(trainingsToShow).map((trainingColumn, idx) =>
+                    <View style={styles.cardColumn} key={idx}>
+                        {trainingColumn.map(training =>
+                            <View style={styles.trainingCard} key={training.id}>
+                                <Image source={trainingImage} style={styles.trainingImage}/>
+                                <View style={styles.infoContainer}>
+                                    <Text style={styles.trainingTitle}>{training.title}</Text>
+                                    <View style={styles.detailsContainer}>
+                                        <Text style={styles.trainingDetail}>{training.duration}</Text>
+                                        <Text style={styles.trainingDetail}>{training.difficulty}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        )}
                     </View>
-                    <View style={styles.trainingCard}>
-                    </View>
-                </View>
-                <View style={styles.cardColumn}>
-                    <View style={styles.trainingCard}>
-                    </View>
-                    <View style={styles.trainingCard}>
-                    </View>
-                </View>
+                )}
             </ScrollView>
         </View>
     )
--- components/Trainings/FavouriteTrainings/style.favourite-trainings.js
+++ components/Trainings/FavouriteTrainings/style.favourite-trainings.js
@@ -4,9 +4,9 @@ import {MEDIUM_GREY, WHITE} from "../../../utils/colors";
 export const styles = StyleSheet.create({
     container: {
         width: "90%",
-        height: 220,
+        // height: 220,
         display: "flex",
-        gap: 10
+        gap: 10,
     },
     title: {
         fontFamily: "Lato_700Bold",
@@ -30,13 +30,47 @@ export const styles = StyleSheet.create({
     },
     trainingCard: {
         width: 200,
-        height: "45%",
+        height: 86,
         borderRadius: 9,
-        backgroundColor: WHITE
+        backgroundColor: WHITE,
+        display: "flex",
+        alignItems: 'center',
+        flexDirection: 'row',
+        gap: 10
     },
     cardColumn: {
         display: "flex",
         gap: 10,
         marginRight: 15,
+    },
+    trainingImage: {
+        width: 72,
+        aspectRatio: 1,
+        borderRadius: 6,
+        marginLeft: 8
+    },
+    infoContainer: {
+        height: 72,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: "flex-start"
+    },
+    trainingTitle: {
+        fontSize: 14,
+        fontFamily: 'Lato_700Bold',
+        color: '#192126'
+    },
+    trainingDetail: {
+        backgroundColor: '#F1F1F1',
+        paddingHorizontal: 6,
+        paddingVertical: 2,
+        borderRadius: 3,
+        fontFamily: 'Lato_400Regular',
+        fontSize: 12,
+        color: '#192126'
+    },
+    detailsContainer: {
+        display: 'flex',
+        gap: 5,
     }
 });
