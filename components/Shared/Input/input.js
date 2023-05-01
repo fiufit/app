87006526b18a191cd8d4389c849e78a4ea219c42
Simon Stein <sstein@fi.uba.ar>
2023-04-30 23:34:36 -0300

    Add autofocus prop to Input component

--- components/Shared/Input/input.js
+++ components/Shared/Input/input.js
@@ -1,57 +1,59 @@
-import { TextInput, View } from "react-native";
+import {TextInput, View} from "react-native";
 
-import { DARK_GREY } from "../../../utils/colors";
-import { React } from "react";
-import { styles } from "./styles.input";
+import {DARK_GREY} from "../../../utils/colors";
+import {React} from "react";
+import {styles} from "./styles.input";
 
 const Input = ({
-  left,
-  right,
-  color,
-  fontSize,
-  height,
-  width,
-  backgroundColor,
-  fontFamily,
-  cursorColor,
-  placeholder,
-  value,
-  onChangeText,
-  secureTextEntry,
-  editable,
-  inputMode,
-  marginTop
-}) => {
-  return (
-    <View
-      style={{
-        ...styles.inputContainer,
-        backgroundColor: backgroundColor ?? "#F7F8F8",
-        height,
-        width,
-        marginTop: marginTop === undefined ? 20 : marginTop
-      }}
-    >
-      <View style={styles.sideContainer}>{left}</View>
-      <TextInput
-        placeholder={placeholder}
-        secureTextEntry={secureTextEntry}
-        value={value}
-        onChangeText={onChangeText}
-        style={{
-          ...styles.input,
-          fontSize,
-          color,
-          fontFamily: fontFamily ?? "Poppins_400Regular",
-          paddingTop: fontFamily ? 0 : 3,
-        }}
-        cursorColor={cursorColor ?? DARK_GREY}
-        editable={editable ?? true}
-        inputMode={inputMode ?? "text"}
-      ></TextInput>
-      <View style={styles.sideContainer}>{right}</View>
-    </View>
-  );
+                   left,
+                   right,
+                   color,
+                   fontSize,
+                   height,
+                   width,
+                   backgroundColor,
+                   fontFamily,
+                   cursorColor,
+                   placeholder,
+                   value,
+                   onChangeText,
+                   secureTextEntry,
+                   editable,
+                   inputMode,
+                   marginTop,
+                   autoFocus
+               }) => {
+    return (
+        <View
+            style={{
+                ...styles.inputContainer,
+                backgroundColor: backgroundColor ?? "#F7F8F8",
+                height,
+                width,
+                marginTop: marginTop === undefined ? 20 : marginTop
+            }}
+        >
+            <View style={styles.sideContainer}>{left}</View>
+            <TextInput
+                placeholder={placeholder}
+                secureTextEntry={secureTextEntry}
+                value={value}
+                onChangeText={onChangeText}
+                style={{
+                    ...styles.input,
+                    fontSize,
+                    color,
+                    fontFamily: fontFamily ?? "Poppins_400Regular",
+                    paddingTop: fontFamily ? 0 : 3,
+                }}
+                cursorColor={cursorColor ?? DARK_GREY}
+                editable={editable ?? true}
+                inputMode={inputMode ?? "text"}
+                autoFocus={autoFocus}
+            ></TextInput>
+            <View style={styles.sideContainer}>{right}</View>
+        </View>
+    );
 };
 
 export default Input;
