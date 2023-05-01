c0568f250cb1c82c5805ca5a2143d703f95d2f56
Simon Stein <sstein@fi.uba.ar>
2023-04-30 23:35:36 -0300

    Handle input change and press in SearchBar component

--- components/Shared/SearchBar/searchBar.js
+++ components/Shared/SearchBar/searchBar.js
@@ -2,25 +2,56 @@ import SearchIcon from "../../../assets/images/general/searchIcon.svg";
 import FilterIcon from "../../../assets/images/general/filterIcon.svg";
 import Input from "../Input/input";
 import {Pressable, View} from "react-native";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "../../../firebase";
 
 
-const SearchBar = ({placeholder, height, width, marginTop, backgroundColor, fontFamily, fontSize, filter, onSearch, onFilterPress}) => {
-    const handleInputChange = (value) => {
-        onSearch(value);
+const SearchBar = ({
+                       placeholder,
+                       height,
+                       width,
+                       marginTop,
+                       backgroundColor,
+                       fontFamily,
+                       fontSize,
+                       filter,
+                       navigation,
+                       redirect,
+                       searchForUsers,
+                       searchForTrainings,
+                       autoFocus,
+                       onSearch
+                   }) => {
+
+
+    const handleInputChange = async (value) => {
+        onSearch(value)
     }
 
-    return <Pressable style={{position: 'relative', width, height}}>
-            <Input placeholder={placeholder}
-                  height={"100%"}
-                  width={"100%"}
-                  marginTop={marginTop}
-                  backgroundColor={backgroundColor}
-                  left={<SearchIcon/>}
-                  right={filter && <FilterIcon onPress={onFilterPress}/>}
-                  fontFamily={fontFamily}
-                  fontSize={fontSize}
-                  onChangeText={handleInputChange}
-            />
+    const handlePress = () => {
+        if(redirect){
+            navigation.navigate({name: 'Search', merge: true, params: {
+                filter,
+                searchForUsers,
+                searchForTrainings
+            }})
+        }
+    }
+
+    return <Pressable style={{position: 'relative', width, height}} onPress={handlePress}>
+        <Input placeholder={placeholder}
+               height={"100%"}
+               width={"100%"}
+               marginTop={marginTop}
+               backgroundColor={backgroundColor}
+               left={<SearchIcon/>}
+               right={filter && <FilterIcon/>}
+               fontFamily={fontFamily}
+               fontSize={fontSize}
+               editable={!redirect}
+               onChangeText={handleInputChange}
+               autoFocus={autoFocus}
+        />
     </Pressable>
 
 }
