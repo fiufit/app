9b880e802ba53904755f5ac5dff93a323acb6a45
Simon Stein <sstein@fi.uba.ar>
2023-05-01 22:10:02 -0300

    Resolve Input conflict

--- components/Shared/Input/input.js
+++ components/Shared/Input/input.js
@@ -1,63 +1,7 @@
 import {TextInput, View} from "react-native";
-
-<<<<<<< HEAD
 import {DARK_GREY} from "../../../utils/colors";
 import {React} from "react";
 import {styles} from "./styles.input";
-
-const Input = ({
-                   left,
-                   right,
-                   color,
-                   fontSize,
-                   height,
-                   width,
-                   backgroundColor,
-                   fontFamily,
-                   cursorColor,
-                   placeholder,
-                   value,
-                   onChangeText,
-                   secureTextEntry,
-                   editable,
-                   inputMode,
-                   marginTop,
-                   autoFocus
-               }) => {
-    return (
-        <View
-            style={{
-                ...styles.inputContainer,
-                backgroundColor: backgroundColor ?? "#F7F8F8",
-                height,
-                width,
-                marginTop: marginTop === undefined ? 20 : marginTop
-            }}
-        >
-            <View style={styles.sideContainer}>{left}</View>
-            <TextInput
-                placeholder={placeholder}
-                secureTextEntry={secureTextEntry}
-                value={value}
-                onChangeText={onChangeText}
-                style={{
-                    ...styles.input,
-                    fontSize,
-                    color,
-                    fontFamily: fontFamily ?? "Poppins_400Regular",
-                    paddingTop: fontFamily ? 0 : 3,
-                }}
-                cursorColor={cursorColor ?? DARK_GREY}
-                editable={editable ?? true}
-                inputMode={inputMode ?? "text"}
-                autoFocus={autoFocus}
-            ></TextInput>
-            <View style={styles.sideContainer}>{right}</View>
-        </View>
-    );
-=======
-import { DARK_GREY } from "../../../utils/colors";
-import { styles } from "./styles.input";
 
 const Input = ({
   left,
@@ -77,6 +21,7 @@ const Input = ({
   inputMode,
   marginTop,
   multiline,
+  autoFocus
 }) => {
   return (
     <View
@@ -106,11 +51,11 @@ const Input = ({
         editable={editable ?? true}
         inputMode={inputMode ?? "text"}
         multiline={multiline ?? false}
+        autoFocus={autoFocus}
       ></TextInput>
       <View style={styles.sideContainer}>{right}</View>
     </View>
   );
->>>>>>> main
 };
 
 export default Input;
