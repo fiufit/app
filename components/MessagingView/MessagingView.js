cf060e65ffb8791244d01a2c93a39e215f013411
fedepenic <fedepenic@hotmail.com>
2023-05-12 15:52:25 -0300

    Added ChatPreview component

--- components/MessagingView/MessagingView.js
+++ components/MessagingView/MessagingView.js
@@ -1,11 +1,16 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 
+import ChatPreview from "../ChatPreview/ChatPreview";
 import MessagingTopBar from "../MessagingTopBar/MessagingTopBar";
 import { styles } from "./styles.MessagingView";
 
 const MessagingView = ({ navigation }) => {
   const handleEditPress = () => {
     // handle edit button press
+  };
+
+  const handleConversationPress = () => {
+    console.log("Conversation pressed!");
   };
 
   return (
@@ -16,6 +21,39 @@ const MessagingView = ({ navigation }) => {
       <View style={styles.messagingTopBarContainer}>
         <MessagingTopBar onEditPress={handleEditPress} />
       </View>
+      <TouchableOpacity
+        onPress={handleConversationPress}
+        style={styles.chatPreviewContainer}
+      >
+        <ChatPreview
+          imageSource={require("../../assets/googleIcon.png")}
+          name="John Doe"
+          lastMessage="Hey, how's it going?"
+          lastMessageTime="10:30 AM"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleConversationPress}
+        style={styles.chatPreviewContainer}
+      >
+        <ChatPreview
+          imageSource={require("../../assets/googleIcon.png")}
+          name="John Doe"
+          lastMessage="Hey, how's it going?"
+          lastMessageTime="10:30 AM"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleConversationPress}
+        style={styles.chatPreviewContainer}
+      >
+        <ChatPreview
+          imageSource={require("../../assets/googleIcon.png")}
+          name="John Doe"
+          lastMessage="Hey, how's it going?"
+          lastMessageTime="10:30 AM"
+        />
+      </TouchableOpacity>
     </ScrollView>
   );
 };
--- components/MessagingView/styles.MessagingView.js
+++ components/MessagingView/styles.MessagingView.js
@@ -4,5 +4,9 @@ export const styles = StyleSheet.create({
   messagingTopBarContainer: {
     width: "80%",
     paddingTop: 40,
+    marginBottom: 15,
+  },
+  chatPreviewContainer: {
+    width: "100%",
   },
 });
