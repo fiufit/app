3d04a1531d11a2326bef1cb7d1ca9c10c171cf68
Simon Stein <sstein@fi.uba.ar>
2023-05-19 18:20:17 -0300

    Add user list as a stack screen to trainings navigation

--- components/TrainingsNavigation/trainingsNavigation.js
+++ components/TrainingsNavigation/trainingsNavigation.js
@@ -1,40 +1,31 @@
-import {createStackNavigator} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import Trainings from "../Trainings/trainings";
 import ViewProfile from "../ViewProfile/viewProfile";
 import SearchView from "../SearchView/searchView";
 import UserList from "../UserList/userList";
+
 const Stack = createStackNavigator();
 
 const TrainingsNavigation = () => {
-
-    return(
-            <Stack.Navigator
-                initialRouteName={"TrainingsView"}
-                screenOptions={{
-                    headerShown: false,
-                    animationEnabled: false
-                }}
-                backBehavior="history"
-            >
-                <Stack.Screen
-                    name={"TrainingsView"}
-                    component={Trainings}
-                />
-                <Stack.Screen
-                    name={"View Profile"}
-                    component={ViewProfile}
-                />
-                <Stack.Screen
-                    name={"Search View"}
-                    component={SearchView}
-                    options={{unmountOnBlur: true}}
-                />
-                <Stack.Screen
-                    name={"View Follows List"}
-                    component={UserList}
-                />
-            </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      initialRouteName={"TrainingsView"}
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: false,
+      }}
+      backBehavior="history"
+    >
+      <Stack.Screen name={"TrainingsView"} component={Trainings} />
+      <Stack.Screen name={"View Profile"} component={ViewProfile} />
+      <Stack.Screen
+        name={"Search View"}
+        component={SearchView}
+        options={{ unmountOnBlur: true }}
+      />
+      <Stack.Screen name={"User List"} component={UserList} />
+    </Stack.Navigator>
+  );
+};
 
 export default TrainingsNavigation;
