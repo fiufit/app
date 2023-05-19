eb220bdef1d526f2f858aad9d289e4683fd083eb
Simon Stein <sstein@fi.uba.ar>
2023-05-19 18:18:53 -0300

    Add user list as a stack screen in profile navigation

--- components/ProfileNavigation/profileNavigation.js
+++ components/ProfileNavigation/profileNavigation.js
@@ -1,28 +1,25 @@
-import {createStackNavigator} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import Profile from "../Profile";
 import UserList from "../UserList/userList";
+import ViewProfile from "../ViewProfile/viewProfile";
+
 const Stack = createStackNavigator();
 
 const ProfileNavigation = () => {
-    return(
-        <Stack.Navigator
-            initialRouteName={"ProfileView"}
-            screenOptions={{
-                headerShown: false,
-                animationEnabled: false
-            }}
-            backBehavior="history"
-        >
-            <Stack.Screen
-                name={"ProfileView"}
-                component={Profile}
-            />
-            <Stack.Screen
-                name={"Follows List"}
-                component={UserList}
-            />
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      initialRouteName={"ProfileView"}
+      screenOptions={{
+        headerShown: false,
+        animationEnabled: false,
+      }}
+      backBehavior="history"
+    >
+      <Stack.Screen name={"ProfileView"} component={Profile} />
+      <Stack.Screen name={"View Profile"} component={ViewProfile} />
+      <Stack.Screen name={"User List"} component={UserList} />
+    </Stack.Navigator>
+  );
+};
 
 export default ProfileNavigation;
