b34e98855dd5e6fcb2e8ba27f04765e9c4022193
Simon Stein <sstein@fi.uba.ar>
2023-05-23 20:35:15 -0300

    Add the posibility to rate a training

--- components/Ratings/ratings.js
+++ components/Ratings/ratings.js
@@ -9,17 +9,24 @@ import RatingModal from "../Shared/Modals/RatingModal/ratingModal";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import ReviewController from "../../utils/controllers/ReviewController";
+import { useRecoilState } from "recoil";
+import { selectedTrainingState } from "../../atoms";
 
 const MAX_RATING = 5;
 
 const Ratings = ({ navigation, route }) => {
   const [user] = useAuthState(auth);
-  const { training } = route.params;
-  const { Name: title, MeanScore} = training;
-  const [meanScore, setMeanScore] = useState(MeanScore);
+  const { training, userTraining } = route.params;
   const [showRatingModal, setShowRatingModal] = useState(false);
   const [reviews, setReviews] = useState([]);
   const [loadingReviews, setLoadingReviews] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [userReview, setUserReview] = useState(null);
+  const [selectedTraining, setSelectedTraining] = useRecoilState(
+    selectedTrainingState
+  );
+  const { Name: title, MeanScore } = userTraining ? training : selectedTraining;
+  const [meanScore, setMeanScore] = useState(MeanScore);
 
   const fetchReviews = async () => {
     const controller = new ReviewController(user);
@@ -27,25 +34,62 @@ const Ratings = ({ navigation, route }) => {
     return data.reviews;
   };
 
-  const handleSubmit = (rating, comment) => {
-    //TODO
+  const handleSubmit = async (rating, comment) => {
+    setSubmitting(true);
+    const controller = new ReviewController(user);
+    if (userReview) {
+      const { data } = await controller.updateReview(
+        selectedTraining.ID,
+        userReview.ID,
+        rating,
+        comment
+      );
+
+      if (data) {
+        const newMeanScore =
+          (meanScore * reviews.length + rating - userReview.Score) /
+          reviews.length;
+        setSelectedTraining({ ...selectedTraining, MeanScore: newMeanScore });
+        setMeanScore(newMeanScore);
+        setReviews(
+          reviews.map((review) => (review.ID === userReview.ID ? data : review))
+        );
+        setUserReview(data);
+      }
+    } else {
+      const { data } = await controller.postReview(
+        selectedTraining.ID,
+        rating,
+        comment
+      );
+      if (data) {
+        const newMeanScore = (meanScore + rating) / (reviews.length + 1);
+        setSelectedTraining({ ...selectedTraining, MeanScore: newMeanScore });
+        setMeanScore(newMeanScore);
+        setReviews([...reviews, data]);
+        setUserReview(data);
+      }
+    }
+    setShowRatingModal(false);
+    setSubmitting(false);
   };
 
   const handleBack = () => {
     navigation.goBack();
-  }
+  };
 
   useEffect(() => {
     if (meanScore > 0) {
       setLoadingReviews(true);
       fetchReviews()
-          .then((reviews) => {
-            setReviews(reviews);
-            setLoadingReviews(false);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .then((reviews) => {
+          setReviews(reviews);
+          setUserReview(reviews.find((review) => review.User.ID === user.uid));
+          setLoadingReviews(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }, []);
 
@@ -54,108 +98,121 @@ const Ratings = ({ navigation, route }) => {
       <View style={styles.container}>
         <Back onPress={handleBack} />
         <Text style={styles.title}>{title}</Text>
-        { meanScore === 0 ?
+        {meanScore === 0 ? (
           <Text style={styles.subtitle}>
-            Be the first to rate this training!
-          </Text> : <View style={styles.ratingContainer}>
-              <Text style={styles.rating}>{meanScore.toFixed(1)}</Text>
-              <StarIcon color={DARK_BLUE} height={50} width={50} />
-            </View>
-        }
-        {meanScore > 0 && <View style={{ height: "50%", width: "100%" }}>
-          <ScrollView
+            {userTraining ? "There are no ratings for this training" : "Be the first to rate this training!"}
+          </Text>
+        ) : (
+          <View style={styles.ratingContainer}>
+            <Text style={styles.rating}>{meanScore.toFixed(1)}</Text>
+            <StarIcon color={DARK_BLUE} height={50} width={50} />
+          </View>
+        )}
+        {meanScore > 0 && (
+          <View style={{ height: userTraining? "60%" : "50%", width: "100%" }}>
+            <ScrollView
               overScrollMode="never"
               showsVerticalScrollIndicator={false}
               style={styles.ratingsContainer}
               contentContainerStyle={{ alignItems: "center", gap: 20 }}
-          >
-            {loadingReviews &&
+            >
+              {loadingReviews &&
                 Array(3)
-                    .fill()
-                    .map((_, index) => {
-                      return (
-                          <View key={index} style={styles.starsAndTextContainer}>
-                            <View style={styles.ratingCard}>
-                              <View style={styles.loadingPicture} />
-                              <View>
-                                <Text style={styles.loadingName}></Text>
-                                <View style={styles.starsContainer}>
-                                  {Array(MAX_RATING)
-                                      .fill()
-                                      .map((_, index) => {
-                                        return (
-                                            <StarIcon
-                                                key={index}
-                                                width={15}
-                                                height={15}
-                                                color={DARK_BLUE}
-                                                opacity={0.2}
-                                            />
-                                        );
-                                      })}
-                                </View>
-                              </View>
-                            </View>
-                            <Text style={styles.loadingRating}></Text>
-                            <Text style={styles.loadingRating}></Text>
-                            <Text style={styles.loadingRatingLast}></Text>
-                          </View>
-                      );
-                    })}
-            {reviews &&
-                reviews?.map((review, index) => {
-                  return (
-                      <View style={styles.starsAndTextContainer} key={index}>
-                        <TouchableOpacity style={styles.ratingCard}>
-                          <Image
-                              source={{ uri: review.User.PictureUrl }}
-                              style={styles.profilePicture}
-                          />
+                  .fill()
+                  .map((_, index) => {
+                    return (
+                      <View key={index} style={styles.starsAndTextContainer}>
+                        <View style={styles.ratingCard}>
+                          <View style={styles.loadingPicture} />
                           <View>
-                            <Text style={styles.name}>
-                              {review.User.DisplayName}
-                            </Text>
+                            <Text style={styles.loadingName}></Text>
                             <View style={styles.starsContainer}>
                               {Array(MAX_RATING)
-                                  .fill()
-                                  .map((_, index) => {
-                                    return (
-                                        <StarIcon
-                                            key={index}
-                                            width={15}
-                                            height={15}
-                                            color={DARK_BLUE}
-                                            opacity={review.Score - 1 >= index ? 1 : 0.2}
-                                        />
-                                    );
-                                  })}
+                                .fill()
+                                .map((_, index) => {
+                                  return (
+                                    <StarIcon
+                                      key={index}
+                                      width={15}
+                                      height={15}
+                                      color={DARK_BLUE}
+                                      opacity={0.2}
+                                    />
+                                  );
+                                })}
                             </View>
                           </View>
-                        </TouchableOpacity>
-                        <Text style={styles.ratingText}>{review.Comment}</Text>
+                        </View>
+                        <Text style={styles.loadingRating}></Text>
+                        <Text style={styles.loadingRating}></Text>
+                        <Text style={styles.loadingRatingLast}></Text>
                       </View>
+                    );
+                  })}
+              {reviews &&
+                reviews?.map((review, index) => {
+                  return (
+                    <View style={styles.starsAndTextContainer} key={index}>
+                      <View style={styles.ratingCard}>
+                        <Image
+                          source={{ uri: review.User.PictureUrl }}
+                          style={styles.profilePicture}
+                        />
+                        <View>
+                          <Text style={styles.name}>
+                            {review.User.DisplayName}
+                          </Text>
+                          <View style={styles.starsContainer}>
+                            {Array(MAX_RATING)
+                              .fill()
+                              .map((_, index) => {
+                                return (
+                                  <StarIcon
+                                    key={index}
+                                    width={15}
+                                    height={15}
+                                    color={DARK_BLUE}
+                                    opacity={
+                                      review.Score - 1 >= index ? 1 : 0.2
+                                    }
+                                  />
+                                );
+                              })}
+                          </View>
+                        </View>
+                      </View>
+                      <Text style={styles.ratingText}>{review.Comment}</Text>
+                    </View>
                   );
                 })}
-          </ScrollView>
-        </View>}
+            </ScrollView>
+          </View>
+        )}
 
-        <View style={styles.buttonContainer}>
-          <Button
-            style={styles.rateButton}
-            fontSize={16}
-            textColor={WHITE}
-            onPress={() => setShowRatingModal(true)}
-          >
-            Rate
-          </Button>
-        </View>
+        {!userTraining && (
+          <View style={styles.buttonContainer}>
+            <Button
+              style={styles.rateButton}
+              fontSize={16}
+              textColor={WHITE}
+              onPress={() => {
+                if (!loadingReviews) {
+                  setShowRatingModal(true);
+                }
+              }}
+            >
+              Rate
+            </Button>
+          </View>
+        )}
       </View>
       {showRatingModal && (
         <RatingModal
-          initialRating={0}
-          initialComment={""}
+          initialRating={userReview?.Score ?? 0}
+          initialComment={userReview?.Comment ?? ""}
           onClose={() => setShowRatingModal(false)}
           onSubmit={handleSubmit}
+          submitting={submitting}
         />
       )}
     </>
