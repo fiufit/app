a320d4ab063ec3aa8a0eaf894c172862ee52c1f7
fedepenic <fedepenic@hotmail.com>
2023-05-27 20:55:53 -0300

    Fetch conversations from Firebase.

--- components/MessagingView/MessagingView.js
+++ components/MessagingView/MessagingView.js
@@ -1,15 +1,38 @@
 import { ScrollView, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
 
 import ChatPreview from "../ChatPreview/ChatPreview";
 import Input from "../Shared/Input/input";
+import MessageController from "../../utils/controllers/MessageController";
 import MessagingTopBar from "../MessagingTopBar/MessagingTopBar";
 import SearchIcon from "../../assets/images/general/searchIcon.svg";
 import { WHITE } from "../../utils/colors";
 import { styles } from "./styles.MessagingView";
-import { useState } from "react";
 
 const MessagingView = ({ navigation }) => {
   const [searchedUser, setSearchedUser] = useState("");
+  const [chatPreviews, setChatPreviews] = useState([]);
+
+  useEffect(() => {
+    const messageController = new MessageController();
+
+    messageController
+      .getConversationsFromUser("pepe")
+      .then((data) => {
+        const newChatPreviews = data.map((item) => ({
+          name: item.members.join(", "),
+          imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
+          lastMessage: "Hey, how is it going?",
+          lastMessageTime: "10:30 AM",
+          hasUnreadMessage: true,
+          conversationId: 1,
+        }));
+        setChatPreviews([...chatPreviews, ...newChatPreviews]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const handleNewMessage = () => {
     navigation.navigate({
@@ -25,105 +48,6 @@ const MessagingView = ({ navigation }) => {
   const handleConversationPress = (conversationId) => {
     navigation.navigate("Conversation", { conversationId: conversationId });
   };
-
-  const chatPreviews = [
-    {
-      name: "Alejandro",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: true,
-      conversationId: 1,
-    },
-    {
-      name: "Alejo",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 2,
-    },
-    {
-      name: "Barbara",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 3,
-    },
-    {
-      name: "Carlos",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 1,
-    },
-    {
-      name: "Carla",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 2,
-    },
-    {
-      name: "Cecilia",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 3,
-    },
-    {
-      name: "Dario",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 1,
-    },
-    {
-      name: "Pedro",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: true,
-      conversationId: 2,
-    },
-    {
-      name: "Pablo",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 3,
-    },
-    {
-      name: "Cristian",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 1,
-    },
-    {
-      name: "Pepe",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 2,
-    },
-    {
-      name: "Jorge",
-      imageSource: "https://randomuser.me/api/portraits/men/75.jpg",
-      lastMessage: "Hey, how is it going?",
-      lastMessageTime: "10:30 AM",
-      hasUnreadMessage: false,
-      conversationId: 3,
-    },
-  ];
 
   return (
     <View style={styles.messagingViewContainer}>
--- utils/controllers/MessageController.js
+++ utils/controllers/MessageController.js
@@ -1,29 +1,46 @@
-import {db} from "../../firebase";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
-class MessageController{
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 
-    constructor() {
-    }
+import { db } from "../../firebase";
 
-    async getConversationById(converstionId){
-        const docRef = doc(db, "conversations", converstionId);
-        const docSnap = await getDoc(docRef);
+class MessageController {
+  constructor() {}
 
-        const data = await docSnap.data();
+  async getConversationById(converstionId) {
+    const docRef = doc(db, "conversations", converstionId);
+    const docSnap = await getDoc(docRef);
 
-        console.log("conversacion", data);
-        return data;
-    }
+    const data = await docSnap.data();
 
-    onGetConversationById(converstionId, onGet){
-        return onSnapshot(doc(db, "conversations", converstionId), (doc) => {
-            const conversationData = doc.data();
+    return data;
+  }
 
-            onGet(conversationData);
-        });
-    }
+  onGetConversationById(converstionId, onGet) {
+    return onSnapshot(doc(db, "conversations", converstionId), (doc) => {
+      const conversationData = doc.data();
 
+      onGet(conversationData);
+    });
+  }
+
+  async getConversationsFromUser(userName) {
+    const conversationsRef = collection(db, "conversations");
+    const q = query(
+      conversationsRef,
+      where("members", "array-contains", userName)
+    );
+    const querySnapshot = await getDocs(q);
+    const conversations = querySnapshot.docs.map((doc) => doc.data());
+
+    return conversations;
+  }
 }
-
 
 export default MessageController;
