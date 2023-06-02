9843a43d9eb25080d23d30455e7204170f3f819c
fedepenic <fedepenic@hotmail.com>
2023-06-02 16:15:31 -0300

    Truncate message in chatPreview.

--- components/ChatPreview/ChatPreview.js
+++ components/ChatPreview/ChatPreview.js
@@ -10,6 +10,19 @@ const ChatPreview = ({
   hasUnreadMessage,
   lastMessageSender,
 }) => {
+  const MAX_CHARACTER_LENGTH = 16;
+  const truncatedLastMessage = truncateLastMessage(
+    lastMessage,
+    MAX_CHARACTER_LENGTH
+  );
+
+  function truncateLastMessage(message, maxLength) {
+    if (message.length > maxLength) {
+      return message.substring(0, maxLength - 3) + "...";
+    }
+    return message;
+  }
+
   return (
     <View
       style={[
@@ -22,14 +35,17 @@ const ChatPreview = ({
         <Text style={styles.textName}>{name}</Text>
         <View style={styles.lastMessageContainer}>
           <Text style={styles.lastMessageSender}>{lastMessageSender}: </Text>
-          <Text
-            style={[
-              styles.lastMessage,
-              hasUnreadMessage && styles.unreadMessage,
-            ]}
-          >
-            {lastMessage}
-          </Text>
+          <View>
+            <Text
+              style={[
+                styles.lastMessage,
+                hasUnreadMessage && styles.unreadMessage,
+              ]}
+              numberOfLines={1}
+            >
+              {truncatedLastMessage}
+            </Text>
+          </View>
         </View>
       </View>
       <Text style={styles.lastMessageTime}>{lastMessageTime}</Text>
