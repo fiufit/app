9ef7bd82d6b07f5b71ab240c5b6b77113c4241d8
Simon Stein <sstein@fi.uba.ar>
2023-06-04 19:39:45 -0300

    Add create, get and update session methods

--- utils/trainings.js
+++ utils/trainings.js
@@ -1,4 +1,14 @@
-export const parseExercises = (exercises) => {
+export const parseExercises = (exercises, session = false) => {
+    if (session) {
+        return exercises.map((exercise) => {
+            return {
+                id: exercise.ID,
+                title: exercise.Exercise.Title,
+                description: exercise.Exercise.Title,
+                done: exercise.Done,
+            };
+        });
+    }
     return exercises.map((exercise) => {
         return {
             exerciseOldData: exercise,
@@ -22,3 +32,25 @@ export const interests = [
     "Lose weight",
     "Gain weight",
 ];
+
+export const getPercentageDone = (session) => {
+    const { ExerciseSessions } = session;
+    const totalExercises = ExerciseSessions.length;
+    const doneExercises = ExerciseSessions.filter(
+        (exercise) => exercise.Done
+    ).length;
+    return Math.round((doneExercises / totalExercises) * 100).toFixed(0);
+};
+
+export const phraseToShow = (session) => {
+    const { StepCount, SecondsCount } = session;
+    if (session.Done) {
+        return "Training done!";
+    } else if (getPercentageDone(session) === "100") {
+        return "Mark as done!";
+    } else if (StepCount === 0 && SecondsCount === 0) {
+        return "Start your training!";
+    } else {
+        return `${StepCount} steps and ${Math.ceil(SecondsCount / 60)} min done`;
+    }
+};
