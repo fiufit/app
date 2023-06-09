0a4d772a3cffc66ce33b6989eb2e9f775ca4b3da
Simon Stein <sstein@fi.uba.ar>
2023-06-08 23:55:47 -0300

    Create LocationModal and update search bar

---
+++ components/Shared/Modals/LocationModal/style.location-modal.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+import {WHITE} from "../../../../utils/colors";
+
+export const styles = StyleSheet.create({
+    title: {
+        fontFamily: "Poppins_500Medium",
+        fontSize: 20,
+        color: "black",
+        textAlign: "center",
+    },
+    modalContainer: {
+        zIndex: 10,
+        backgroundColor: "rgba(0,0,0,0.34)",
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    modal: {
+        width: "90%",
+        height: 200,
+        padding: "10%",
+        backgroundColor: WHITE,
+        borderRadius: 15,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10
+    },
+    button: {
+        width: "100%",
+        height: 40,
+        borderRadius: 15,
+        marginTop: 20
+    },
+});
