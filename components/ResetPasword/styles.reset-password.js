74680d79d732b4f75834a488484279047eedc8c3
Simon Stein <sstein@fi.uba.ar>
2023-06-15 22:23:43 -0300

    Add reset password component

---
+++ components/ResetPasword/styles.reset-password.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    title: {
+        fontSize: 16,
+        fontFamily: "Poppins_500Medium",
+    },
+    subtitle: {
+        fontSize: 20,
+        fontFamily: "Poppins_700Bold",
+    },
+    textInput: {
+        width: "80%",
+        height: 30,
+        marginTop: 20,
+    },
+    button: {
+        height: 40,
+        borderRadius: 10,
+        width: "80%",
+    },
+    textContainer: {
+        display: "flex",
+        alignItems: "center",
+    }
+});
