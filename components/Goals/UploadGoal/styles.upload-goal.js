ae98823696c9a0276b18c7811c94a73ab0540fab
Simon Stein <sstein@fi.uba.ar>
2023-06-19 02:14:08 -0300

    Create upload goal component

---
+++ components/Goals/UploadGoal/styles.upload-goal.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+import { GREY, WHITE } from "../../../utils/colors";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: WHITE,
+        position: "relative",
+    },
+    title: {
+        fontFamily: "Poppins_500Medium",
+        fontSize: 22,
+        width: "80%",
+        marginBottom: 10
+    },
+    subTitle: {
+        fontFamily: "Poppins_500Medium",
+        fontSize: 18,
+        width: "80%",
+    },
+    buttonsContainer: {
+        width: "80%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    categoryButton: {
+        width: "30%",
+        borderRadius: 10
+    },
+    inputContainer: {
+        width: "80%",
+    },
+    buttonContainer: {
+        position: "absolute",
+        width: "100%",
+        height: 45,
+        bottom: 40,
+        display: 'flex',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    createButton: {
+        height: "100%",
+        borderRadius: 10,
+        width: '85%',
+    }
+});
