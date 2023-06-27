da6eac7d8d80571518bbcb8a96da21cd7284e036
Simon Stein <sstein@fi.uba.ar>
2023-06-26 22:35:02 -0300

    Add whatsapp verification

--- components/AuthenticationWrapper/EmailVerification/styles.email-verification.js
+++ components/AuthenticationWrapper/EmailVerification/styles.email-verification.js
@@ -1,6 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+    topContainer: {
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        gap: 10,
+        marginTop: "30%",
+    },
     title: {
         fontFamily: "Poppins_500Medium",
         fontSize: 15
@@ -8,6 +15,42 @@ export const styles = StyleSheet.create({
     verifyButton: {
         height: 40,
         borderRadius: 10,
+        width: "100%",
+    },
+    optionContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
         width: "80%",
+
+    },
+    optionButton: {
+        height: 30,
+        width: "40%",
+        borderRadius: 10,
+    },
+    mailOptionContainer:{
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        width: "80%",
+        gap: 20,
+        height: "30%",
+
+    },
+    whatsappOptionContainer: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: "70%",
+        gap: 10,
+        height: "30%",
+    },
+    whatsappOptionTitle: {
+        fontFamily: "Poppins_500Medium",
+        fontSize: 14,
+        alignSelf: "flex-start",
+        paddingHorizontal: 20
     }
 });
