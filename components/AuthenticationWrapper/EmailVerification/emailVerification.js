c3db242e9fe0837251be1513f3202086563b7138
Simón Stein <86434696+stein257@users.noreply.github.com>
2023-06-28 19:31:03 -0300

    99 whatsapp verification (#101)
    * Add phone icon
    
    * Add PIN verification methods to auth controller
    
    * Add whatsapp verification

--- components/AuthenticationWrapper/EmailVerification/emailVerification.js
+++ components/AuthenticationWrapper/EmailVerification/emailVerification.js
@@ -3,16 +3,28 @@ import Background from "../../Background/background";
 import Button from "../../Shared/Button/button";
 import ErrorModal from "../../Shared/Modals/ErrorModal/ErrorModal";
 import SuccessModal from "../../Shared/Modals/SuccessModal/SuccessModal";
-import { Text } from "react-native";
-import { WHITE } from "../../../utils/colors";
+import { Text, View } from "react-native";
+import { BLACK, DARK_BLUE, WHITE } from "../../../utils/colors";
 import { styles } from "./styles.email-verification";
 import { useState } from "react";
+import LogOutButton from "../../Shared/LogOutButton/logOutButton";
+import Input from "../../Shared/Input/input";
+import PhoneIcon from "../../../assets/images/general/phone-portrait-outline.svg";
+import LockIcon from "../../../assets/images/general/lockIcon.svg";
 
 const EmailVerification = ({ user }) => {
   const [errorModalIsVisible, setErrorModalIsVisible] = useState(false);
   const [errorDescription, setErrorDescription] = useState("");
   const [successModalIsVisible, setSuccessModalIsVisible] = useState(false);
   const [successDescription, setSuccessDescription] = useState("");
+  const [mailOptionSelected, setMailOptionSelected] = useState(true);
+  const [phoneNumber, setPhoneNumber] = useState("+549");
+  const [pin, setPin] = useState("");
+  const [sending, setSending] = useState(false);
+  const [verifying, setVerifying] = useState(false);
+  const [pinError, setPinError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+
   const handleSendVerifyLink = async () => {
     const controller = new AuthenticationController(user);
     const { email } = await controller.sendVerificationMail();
@@ -32,36 +44,152 @@ const EmailVerification = ({ user }) => {
     await user.getIdToken(true);
   };
 
+  const handleSendVerifyPin = async () => {
+    if(!phoneNumber){
+        setPhoneError("Please enter a phone number");
+        return;
+    }
+
+    if (phoneNumber && !sending) {
+      setSending(true);
+      const controller = new AuthenticationController(user);
+      const { data, error } = await controller.sendVerificationPin(phoneNumber);
+      console.log(data);
+      if (error) {
+        setErrorModalIsVisible(true);
+        setErrorDescription(
+          "There was an error sending the pin, please verify your number and try again."
+        );
+      }
+      setSending(false);
+    }
+  };
+
+  const handleVerifyPin = async () => {
+    if(!pin){
+        setPinError("Please enter a pin");
+        return;
+    }
+    if(pin && !verifying){
+      setVerifying(true)
+      const controller = new AuthenticationController(user);
+      const { data, error } = await controller.verifyPin(pin);
+      console.log(data);
+      if (error) {
+        console.log(error);
+        setErrorModalIsVisible(true);
+        setErrorDescription(
+            "There was an error verifying the pin, please verify it and try again."
+        );
+      } else {
+        await handleAlreadyVerify();
+      }
+      setVerifying(false)
+    }
+  };
+
   return (
     <Background
       fromColor={"rgb(185, 213, 123)"}
       toColor={"rgb(254,254,253)"}
       styles={{
         display: "flex",
-        justifyContent: "space-evenly",
         alignItems: "center",
         height: "100%",
         flexDirection: "column",
+        gap: 80,
       }}
     >
-      <Text style={styles.title}>Please, verify your email</Text>
-      <Button
-        textColor={WHITE}
-        fontSize={16}
-        style={styles.verifyButton}
-        onPress={handleSendVerifyLink}
-      >
-        Resend verification link
-      </Button>
-
-      <Button
-        textColor={WHITE}
-        fontSize={16}
-        style={styles.verifyButton}
-        onPress={handleAlreadyVerify}
-      >
-        I already verified my email
-      </Button>
+      <LogOutButton />
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>Please, verify your account</Text>
+        <View style={styles.optionContainer}>
+          <Button
+            textColor={mailOptionSelected ? WHITE : DARK_BLUE}
+            buttonColor={mailOptionSelected ? DARK_BLUE : "transparent"}
+            style={styles.optionButton}
+            onPress={() => setMailOptionSelected(true)}
+          >
+            by email
+          </Button>
+          <Button
+            textColor={!mailOptionSelected ? WHITE : DARK_BLUE}
+            buttonColor={!mailOptionSelected ? DARK_BLUE : "transparent"}
+            style={styles.optionButton}
+            onPress={() => setMailOptionSelected(false)}
+          >
+            by whastapp
+          </Button>
+        </View>
+      </View>
+      {mailOptionSelected ? (
+        <View style={styles.mailOptionContainer}>
+          <Button
+            textColor={WHITE}
+            fontSize={16}
+            style={styles.verifyButton}
+            onPress={handleSendVerifyLink}
+          >
+            Resend verification link
+          </Button>
+          <Button
+            textColor={WHITE}
+            fontSize={16}
+            style={styles.verifyButton}
+            onPress={handleAlreadyVerify}
+          >
+            I already verified my email
+          </Button>
+        </View>
+      ) : (
+        <View style={styles.whatsappOptionContainer}>
+          <Text style={styles.whatsappOptionTitle}>
+            Enter your phone number
+          </Text>
+          <Input
+            onChangeText={(text) => {
+              setPhoneError("");
+              setPhoneNumber(text);
+            }}
+            placeholder={phoneError ? phoneError : "Phone number"}
+            width={"100%"}
+            height={40}
+            value={phoneNumber}
+            marginTop={0}
+            left={<PhoneIcon width={30} height={20} color={BLACK} />}
+            placeholderTextColor={phoneError ? "red" : null}
+          />
+          <Button
+            onPress={handleSendVerifyPin}
+            textColor={WHITE}
+            fontSize={16}
+            style={styles.verifyButton}
+          >
+            {sending ? "Sending..." : "Send verification PIN"}
+          </Button>
+          <Input
+            placeholder={pinError ? pinError : "PIN"}
+            width={"100%"}
+            height={40}
+            marginTop={40}
+            left={<LockIcon color={BLACK} />}
+            onChangeText={(text) => {
+              setPinError("");
+              setPin(text);
+            }}
+            value={pin}
+            placeholderTextColor={pinError ? "red" : null}
+          />
+          <Button
+            textColor={WHITE}
+            fontSize={16}
+            style={styles.verifyButton}
+            onPress={handleVerifyPin}
+          >
+            {verifying ? "Verifying..." : "Verify"}
+          </Button>
+        </View>
+      )}
       <ErrorModal
         modalIsVisible={errorModalIsVisible}
         setModalIsVisible={setErrorModalIsVisible}
--- utils/controllers/AuthenticationController.js
+++ utils/controllers/AuthenticationController.js
@@ -1,61 +1,79 @@
-import {singIn} from "../../firebase";
+import { singIn } from "../../firebase";
 import RequestController from "./RequestController";
 import { FIREBASE_EMAIL_VERIFY_URL } from "@env";
 
-
 const emailVerifyUrl = FIREBASE_EMAIL_VERIFY_URL;
 
-class AuthenticationController{
-    constructor(user) {
-        this.user = user;
-        this.requestController = new RequestController(user);
+class AuthenticationController {
+  constructor(user) {
+    this.user = user;
+    this.requestController = new RequestController(user);
+  }
+
+  async sendVerificationMail() {
+    const { stsTokenManager } = this.user;
+    const response = await fetch(emailVerifyUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        requestType: "VERIFY_EMAIL",
+        idToken: stsTokenManager.accessToken,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+
+  async startRegister(email, password) {
+    const data = await this.requestController.fetch("users/register", "POST", {
+      email: email.trim(),
+      password: password.trim(),
+    });
+
+    if (data.error) {
+      throw data.error;
+    } else {
+      await singIn(email, password);
     }
+  }
 
-    async sendVerificationMail(){
-        const {stsTokenManager} = this.user;
-        //TODO: add apikey to .env file
-        const response = await fetch(emailVerifyUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                requestType: "VERIFY_EMAIL",
-                idToken: stsTokenManager.accessToken
-            })
-        });
-        const data = await response.json()
-        console.log(data)
-        return data;
+  async finishRegister(userData) {
+    const data = await this.requestController.fetch(
+      "users/finish-register",
+      "POST",
+      userData
+    );
+
+    console.log(data);
+    if (data.error) {
+      throw data.error;
+    } else {
+      return data;
     }
+  }
 
-    async startRegister(email, password){
-        const data = await this.requestController
-            .fetch('users/register', "POST", {
-                email: email.trim(),
-                password: password.trim()
-            });
+  async sendVerificationPin(phoneNumber) {
+    return await this.requestController.fetch(
+      `users/${this.user.uid}/verification/send`,
+      "POST",
+      {
+        phone_number: phoneNumber,
+      }
+    );
+  }
 
-        if(data.error){
-            throw data.error
-        } else {
-            await singIn(email, password);
-        }
-    }
-
-    async finishRegister(userData){
-        const data = await this.requestController
-            .fetch('users/finish-register', "POST", userData);
-
-        console.log(data)
-        if(data.error){
-            throw data.error
-        } else {
-            return data;
-        }
-    }
-
-
+  async verifyPin(pin) {
+    return await this.requestController.fetch(
+      `users/${this.user.uid}/verification/verify`,
+      "POST",
+      {
+        pin,
+      }
+    );
+  }
 }
 
 export default AuthenticationController;
