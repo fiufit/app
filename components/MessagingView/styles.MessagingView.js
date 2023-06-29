78fab1343675026240e9631b70840ec88c5675b7
Simon Stein <sstein@fi.uba.ar>
2023-06-28 22:22:16 -0300

    Unify styles of messages screen with app

--- components/MessagingView/styles.MessagingView.js
+++ components/MessagingView/styles.MessagingView.js
@@ -1,12 +1,19 @@
 import { StyleSheet } from "react-native";
+import {GREY} from "../../utils/colors";
 
 export const styles = StyleSheet.create({
   messagingViewContainer: {
     alignItems: "center",
+    height: "100%",
+    backgroundColor: GREY,
+  },
+  scrollContainer:{
+    width: "100%",
+    height: "67%",
   },
   chatPreviewList: {
     width: "100%",
-    height: "67%",
+    height: "100%",
   },
   messagingTopBarContainer: {
     width: "80%",
